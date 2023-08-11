37e7e2baa8154f60b9410cac9b513a283de2a229
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2023-08-11 21:40:00 +0300

    Добавляет AddButtonView (кнопка добавления точки маршрута)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
@@ -7,9 +8,15 @@ import './views/filter-view.js';
 const briefView = document.querySelector('brief-view');
 
 /**
- * @type {import('./views/brief-view').default}
+ * @type {import('./views/filter-view').default}
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+const addButtonView = document.querySelector('add-button-view');
+
 briefView.render();
 filterView.render();
+addButtonView.render();
